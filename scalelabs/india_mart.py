72ea18c4a39aa574668a46f7eb0b0d2dfe80e4ba
sgmonusg <sagarguptamonu1996@gmail.com>
2017-03-20 16:20:21 +0530

    added beautiful dependencies

--- scalelabs/india_mart.py
+++ scalelabs/india_mart.py
@@ -1,27 +1,33 @@
 import urllib2
 import re
 website  = urllib2.urlopen("https://dir.indiamart.com/impcat/mens-suits.html")
-html = wesite.read()
+html = website.read()
 links = re.findall('"((http|ftp)s?://.*?)"', html)
 
-//check
-//checked working
+#//check
+#//checked working
 print links
 
-//for backup got the links in  the file
+#//for backup got the links in  the file
 f = open("in_backup_links.txt", "w")
 for i in links:
 	f.write(str(i))
 f.close()
 
-//the func is not to be used in gen only once to be run  to create the backup
+#//the func is not to be used in gen only once to be run  to create the backup
 
 
-// parsing each link for data using beutiful soup
+#// parsing each link for data using beutiful soup
 
-//import beatiful soup
+#//import beatiful soup
 from bs4 import BeautifulSoup
-soup = BeautifulSoup(html_doc, 'html.parser')
+
+#soup = BeautifulSoup("<html>data</html>")
 for i in links:
-	print(soup.prettify())
+	soup = BeautifulSoup(urllib2.urlopen((str(i[0]))))
+	#//print(soup.prettify())
+	divs =soup.find_all("div", { "class" : "pr10 f1 fs18"})                                       
+	for j in divs:
+		print j
+
 
